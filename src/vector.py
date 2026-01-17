f6bea551757d6b4c544caed1bfea1696ff446c65
Enzo1612 <enzonicaise.1612@gmail.com>
2026-01-17 15:12:54 +0100

    feat: implement Vector class with dot product and norm

---
+++ src/vector.py
@@ -0,0 +1,40 @@
+from math import sqrt
+from typing import List
+
+
+class Vector:
+    def __init__(self, coor:List[float]):
+        self.coor = list(coor)
+    
+    def check_len(self, v2: 'Vector'):
+        if (len(self.coor) != len(v2.coor)):
+            raise ValueError()
+        
+    def __repr__(self):
+        return f"Vector({self.coor})"
+        
+    def __eq__(self, v2: 'Vector'):
+        return self.coor == v2.coor
+
+    def __add__(self, v2: 'Vector') -> 'Vector':
+        self.check_len(v2)
+        return Vector([a + b for a, b in zip(self.coor, v2.coor)])
+
+    def __sub__(self, v2:'Vector') -> 'Vector':
+        self.check_len(v2)
+        return Vector([a - b for a, b in zip(self.coor, v2.coor)])
+
+    def __mul__(self, scalar: float) -> 'Vector':
+        return Vector([a * scalar for a in self.coor])
+
+    def __rmul__(self, scalar:float) -> 'Vector':
+        return self.__mul__(scalar)
+
+    def dot(self, v2: 'Vector') -> float:
+        self.check_len(v2)
+        return sum(a * b for a, b in zip(self.coor, v2.coor))
+
+    @property
+    def norm(self) -> float:
+        return sqrt(self.dot(self))
+
---
+++ tests/test_vector.py
@@ -0,0 +1,28 @@
+import pytest
+from src.vector import Vector
+
+def test_add():
+    v1 = Vector([1, 2])
+    v2 = Vector([3, 4])
+    result = v1 + v2
+    assert result == Vector([4, 6])
+
+def test_mul():
+    v = Vector([1, -2])
+    assert v * 3 == Vector([3, -6])
+    assert 3 * v == Vector([3, -6])
+
+def test_dot():
+    v = Vector([1, 2])
+    w = Vector([3, 4])
+    assert v.dot(w) ==  11
+
+def test_norm():
+    v = Vector([3, 4])
+    assert v.norm == 5.0
+
+def test_error_handling():
+    v1 = Vector([1, 2])
+    v2 = Vector([1, 2, 3])
+    with pytest.raises(ValueError):
+        v1 + v2
